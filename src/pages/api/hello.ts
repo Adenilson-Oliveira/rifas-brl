50c5d3a839be1e3b4318043b8396f274e8d4a1bc
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2022-11-14 07:14:28 -0300

    create checkout session no stripe

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const priceId = ''
+  const quantidade = 5
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: '/',
+    cancel_url: '/',
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: quantidade,
+      },
+    ],
+  })
+
+  console.log(checkoutSession)
+
+  res.status(200).json({ name: 'Hello World!' })
 }
